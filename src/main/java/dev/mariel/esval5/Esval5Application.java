8ffff809158fd7e6ec8debffd7887738b5d48e7c
Codebreakerblack <blancohmariel@gmail.com>
2025-01-10 15:41:40 +0100

    first commit

---
+++ src/main/java/dev/mariel/esval5/Esval5Application.java
@@ -0,0 +1,13 @@
+package dev.mariel.esval5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esval5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esval5Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mariel/esval5/Esval5ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mariel.esval5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esval5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
